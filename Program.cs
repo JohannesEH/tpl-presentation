5c1acd0cfb26c9ea6b8d72b6338398e4896ac75f
Johannes Hansen <johannes.hansen@eksponent.com>
2018-06-19 12:54:18 +0200

    Update code with example

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
 using static System.Console;
@@ -9,15 +10,80 @@ namespace tpl_presentation
     {
         static async Task Main(string[] args)
         {
-            var bb = new BroadcastBlock<int>(c => c);
-            var log = new ActionBlock<int>(x => WriteLine($"Log: {x}"));
-            var save = new ActionBlock<int>(x => WriteLine($"Save: {x}"));
-            bb.LinkTo(log);
-            bb.LinkTo(save);
+            var linkOptions = new DataflowLinkOptions { PropagateCompletion = true };
 
-            await bb.SendAsync(2);
+            #region Setup Dataflow Blocks
+            var ordersBuffer = new BufferBlock<int>(new DataflowBlockOptions { BoundedCapacity = 20 });
 
-            await Task.Delay(3000);
+            var cutDough = new TransformBlock<int, Pizza>(
+                orderId => {
+                    Task.Delay(371).Wait();
+                    return new Pizza(orderId);
+                },
+                new ExecutionDataflowBlockOptions { BoundedCapacity = 1, MaxDegreeOfParallelism = 1 }
+            );
+
+            var addIngredients = new TransformBlock<Pizza, Pizza>(
+                p => {
+                    Task.Delay(457).Wait();
+                    p.Ingredients.Add("Sauce");
+                    p.Ingredients.Add("Pepperoni");
+                    p.Ingredients.Add("Cheese");
+                    return p;
+                },
+                new ExecutionDataflowBlockOptions { BoundedCapacity = 2, MaxDegreeOfParallelism = 2 }
+            );
+
+            var bakePizza = new TransformBlock<Pizza, Pizza>(
+                p => {
+                    Task.Delay(3972).Wait();
+                    p.Baked = true;
+                    return p;
+                },
+                new ExecutionDataflowBlockOptions { BoundedCapacity = 8, MaxDegreeOfParallelism = 8 }
+            );
+
+            var batchPizza = new BatchBlock<Pizza>(5, new GroupingDataflowBlockOptions { BoundedCapacity = 5 });
+
+            var deliverPizzas = new ActionBlock<Pizza[]>(
+                pb => {
+                    Task.Delay(1213 * pb.Length).Wait();
+                    WriteLine($"Delivered {pb.Length} pizzas");
+                },
+                new ExecutionDataflowBlockOptions { BoundedCapacity = 3, MaxDegreeOfParallelism = 3 }
+            );
+            #endregion
+
+            #region Setup Pipeline
+            ordersBuffer.LinkTo(cutDough, linkOptions);
+            cutDough.LinkTo(addIngredients, linkOptions);
+            addIngredients.LinkTo(bakePizza, linkOptions);
+            bakePizza.LinkTo(batchPizza, linkOptions);
+            batchPizza.LinkTo(deliverPizzas, linkOptions);
+            #endregion
+
+            #region Add Orders
+            for (int i = 1; i <= 100; i++)
+            {
+                await ordersBuffer.SendAsync(i);
+            }
+            #endregion
+
+            await deliverPizzas.Completion;
         }
+    }
+
+    public class Pizza
+    {
+        public Pizza(int orderId)
+        {
+            this.OrderId = orderId;
+            this.Ingredients = new List<string>();
+            this.Baked = false;
+        }
+
+        public int OrderId { get; set; }
+        public List<string> Ingredients { get; set; }
+        public bool Baked { get; set; }
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,38 @@
-´´´shell
+# TPL Dataflow
+
+```shell
 dotnet add package System.Threading.Tasks.Dataflow
-´´´
+```
+
+## Block Types
+
+**Normal**
+* ActionBlock
+* BatchBlock
+* BufferBlock
+* TransformBlock
+* TransformManyBlock
+
+**Special**
+* BetchedJoinBlock
+* JoinBlock
+* WriteOnceBlock
+
+
+## Pizza Flow
+
+```mermaid
+graph LR;
+
+TakeOrder -- OrderId --> CutDough
+CutDough -- Pizza --> AddIngredients
+AddIngredients -- Pizza --> Bake
+Bake -- Pizza --> Batch
+Batch -- PizzaBatch --> Deliver
+```
+
+## Topics
+
+* Backpressure
+* Parallelism
+* 
